93d126e53f920fbfbd41924400e98f8d37890d4d
Pedro PSI <36260325+pedropsi@users.noreply.github.com>
2021-06-12 16:54:37 +0100

    sfx database

---
+++ sfx-catalog.js
@@ -0,0 +1,274 @@
+SFXCatalog={
+	"24017806":{
+		"name":"PlayD4",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"9388606":{
+		"name":"PlayE4",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"35099506":{
+		"name":"PlayGb4",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"870706":{
+		"name":"PlayA4",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"1891306":{
+		"name":"PlayB4",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"2507306":{
+		"name":"PlayD5",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"997506":{
+		"name":"PlayE5",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"2966606":{
+		"name":"PlayGb5",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"7959606":{
+		"name":"PlayA5",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"10155006":{
+		"name":"PlayB5",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"2273406":{
+		"name":"PlayD6",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"1882506":{
+		"name":"PlayE6",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"36206":{
+		"name":"PlayGb6",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"4082706":{
+		"name":"PlayA6",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"381406":{
+		"name":"PlayB6",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"1310106":{
+		"name":"PlayD7",
+		"tags":[ "music","tonal"],
+		"finder":"lucas schueller"
+	},
+	"73234504":{
+		"name":"kick",
+		"tags":[ "music","beat","drums"],
+		"finder":"isaac schankler"
+	},
+	"98762304":{
+		"name":"snare",
+		"tags":[ "music","beat","drums"],
+		"finder":"isaac schankler"
+	},
+	"52166707":{
+		"name":"tom",
+		"tags":[ "music","beat","drums"],
+		"finder":"isaac schankler"
+	},
+	"89199102":{
+		"name":"bass",
+		"tags":[ "music","beat","drums"],
+		"finder":"isaac schankler"
+	},
+	"17662500":{
+		"name":"cowbell",
+		"tags":[ "music","beat","drums"],
+		"finder":"isaac schankler"
+	},
+	"50712304":{
+		"name":"punchy kick",
+		"tags":[ "music","beat","drums"],
+		"finder":"isaac schankler"
+	},
+	"82276300":{
+		"name":"light cowbell",
+		"tags":[ "music","beat","drums"],
+		"finder":"isaac schankler"
+	},
+	"70803708":{
+		"name":"sea waves",
+		"tags":[ "nature","sea"],
+		"finder":"pedropsi"
+	},
+	"26722908":{
+		"name":"sea waves",
+		"tags":[ "nature","sea"],
+		"finder":"pedropsi"
+	},
+	"40103708":{
+		"name":"fax machine (not working)",
+		"tags":[ "machine","electronics"],
+		"finder":"pedropsi"
+	},
+	"31760108":{
+		"name":"photocopier",
+		"tags":[ "machine","electronics"],
+		"finder":"pedropsi"
+	},
+	"35304708":{
+		"name":"photocopier, lower",
+		"tags":[ "machine","electronics"],
+		"finder":"pedropsi"
+	},
+	"67283708":{
+		"name":"elevator ping",
+		"tags":[ "machine","electronics"],
+		"finder":"pedropsi"
+	},
+	"1744708":{
+		"name":"modem (not working)",
+		"tags":[ "machine","electronics"],
+		"finder":"pedropsi"
+	},
+	"34240508":{
+		"name":"helicopter leaving",
+		"tags":[ "machine","electronics"],
+		"finder":"pedropsi"
+	},
+	"31567508":{
+		"name":"robot disagreeing",
+		"tags":[ "robot","cancel"],
+		"finder":""
+	},
+	"10959108":{
+		"name":"robot agreeing gently",
+		"tags":[ "robot","confirm"],
+		"finder":""
+	},
+	"60535908":{
+		"name":"wiping clean glass",
+		"tags":[],
+		"finder":"pedropsi"
+	},
+	"77077708":{
+		"name":"woop",
+		"tags":[],
+		"finder":"pedropsi"
+	},
+	"85012908":{
+		"name":"quick digits in succession",
+		"tags":[],
+		"finder":""
+	},
+	"41203708":{
+		"name":"alien bird",
+		"tags":["alien"],
+		"finder":""
+	},
+	"5562908":{
+		"name":"aliens singing",
+		"tags":["alien"],
+		"finder":""
+	},
+	"63440108":{
+		"name":"alien alarm",
+		"tags":["alien"],
+		"finder":""
+	},
+	"16585708":{
+		"name":"bird water robot",
+		"tags":[""],
+		"finder":""
+	},
+	"38947308":{
+		"name":"bird repetitive",
+		"tags":[],
+		"finder":""
+	},
+	"8689508":{
+		"name":"bird chirp",
+		"tags":[],
+		"finder":""
+	},
+	"70103708":{
+		"name":"metallic plate blowing up far away",
+		"tags":["metal"],
+		"finder":"pedropsi"
+	},
+	"34007108":{
+		"name":"heartbeat with delay",
+		"tags":[ "heartbeat"],
+		"finder":"pedropsi"
+	},
+	"57146708":{
+		"name":"heartbeats 2",
+		"tags":[ "heartbeat"],
+		"finder":"pedropsi"
+	},
+	"80103708":{
+		"name":"heartbeats nervous",
+		"tags":[ "heartbeat"],
+		"finder":"pedropsi"
+	},
+	"26680708":{
+		"name":"heartbeats fibrillation",
+		"tags":[ "heartbeat"],
+		"finder":"pedropsi"
+	},
+	"24624508":{
+		"name":"heartbeats running",
+		"tags":[ "heartbeat"],
+		"finder":"pedropsi"
+	},
+	"72755308":{
+		"name":"distant instrument",
+		"tags":[],
+		"finder":""
+	},
+	"85480508":{
+		"name":"instrument",
+		"tags":[],
+		"finder":""
+	},
+	"12814508":{
+		"name":"instrument",
+		"tags":[],
+		"finder":""
+	},
+	"9806908":{
+		"name":"crystal",
+		"tags":[],
+		"finder":""
+	},
+	"73035308":{
+		"name":"metallic",
+		"tags":[],
+		"finder":""
+	},
+	"64018708":{
+		"name":"glassy column",
+		"tags":[],
+		"finder":""
+	}
+}
+
+DefinedShout("sfx-catalog")
